a64a8a27f79881338295d112345d72f38cf7d059
Kirill Müller <kirill@cynkra.com>
2023-08-12 11:37:56 +0200

    Style

--- run_all_queries_and_graph.R
+++ run_all_queries_and_graph.R
@@ -2,7 +2,7 @@
 options(conflicts.policy = list(warn = FALSE))
 library(tidyverse)
 
-source('dplyr/run_all_queries.R')
+source("dplyr/run_all_queries.R")
 
 # remove the data so duckplyr can reload it lazily.
 rm(taxi_data_2019)
@@ -10,19 +10,24 @@ rm(zone_map)
 
 source("duckplyr/run_all_queries.R")
 
-all_timings = c(q1_duckplyr, q2_duckplyr, q3_duckplyr, q4_duckplyr,
-             q1_dplyr, q2_dplyr, q3_dplyr, q4_dplyr)
+all_timings <- c(
+  q1_duckplyr, q2_duckplyr, q3_duckplyr, q4_duckplyr,
+  q1_dplyr, q2_dplyr, q3_dplyr, q4_dplyr
+)
 
-timings = data.frame(query=c("query 1", "query 2", "query 3", "query 4"),
-				time=all_timings,
-				system=c("duckplyr", "duckplyr", "duckplyr", "duckplyr", "dplyr", "dplyr", "dplyr", "dplyr"))
+timings <- data.frame(
+  query = c("query 1", "query 2", "query 3", "query 4"),
+  time = all_timings,
+  system = c("duckplyr", "duckplyr", "duckplyr", "duckplyr", "dplyr", "dplyr", "dplyr", "dplyr")
+)
 
-bargraph <- ggplot(timings, aes(x=query, y=time, fill = system)) +
-          geom_col(position="dodge") + theme(text=element_text(size=20))
+bargraph <- ggplot(timings, aes(x = query, y = time, fill = system)) +
+  geom_col(position = "dodge") +
+  theme(text = element_text(size = 20))
 
 # save the data
-write.csv(timings, "timings.csv", row.names=FALSE)
+write.csv(timings, "timings.csv", row.names = FALSE)
 
 # plot
-dpi = 96
-ggsave(filename = "timings.jpg", plot = bargraph, width=900/dpi, height=500/dpi, dpi=dpi)
+dpi <- 96
+ggsave(filename = "timings.jpg", plot = bargraph, width = 900 / dpi, height = 500 / dpi, dpi = dpi)
