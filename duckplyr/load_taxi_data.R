c0cd3e442001fad7d8139fc1203d81d2f7696294
Kirill Müller <kirill@cynkra.com>
2023-08-12 12:24:17 +0200

    duckplyr one year

--- duckplyr/load_taxi_data.R
+++ duckplyr/load_taxi_data.R
@@ -1,5 +1,5 @@
 if (!exists("taxi_data_2019") && !exists("zone_map")) {
-  taxi_data_2019 <- as_duckplyr_df(duckdb:::sql("FROM 'taxi-data-2019-partitioned/*/*.parquet' WHERE month == 12"))
+  taxi_data_2019 <- as_duckplyr_df(duckdb:::sql("FROM 'taxi-data-2019-partitioned/*/*.parquet'"))
   zone_map <- as_duckplyr_df(duckdb:::sql("FROM 'zone_lookups.parquet'"))
   taxi_data_2019_lazy <- duckplyr_df_from_file("taxi-data-2019-partitioned/*/*.parquet", "read_parquet", list(hive_partitioning = TRUE))
 }
