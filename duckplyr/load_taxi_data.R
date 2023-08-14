3ab8c6b4cdc35ca41c0bb7e283877da79474c9bc
Kirill Müller <kirill@cynkra.com>
2023-08-14 09:40:54 +0200

    Set environment variables

--- duckplyr/load_taxi_data.R
+++ duckplyr/load_taxi_data.R
@@ -3,3 +3,7 @@ if (!exists("taxi_data_2019") && !exists("zone_map")) {
   zone_map <- as_duckplyr_df(duckdb:::sql("FROM 'zone_lookups.parquet'"))
   taxi_data_2019_lazy <- duckplyr_df_from_file("taxi-data-2019-partitioned/*/*.parquet", "read_parquet", list(hive_partitioning = TRUE))
 }
+
+Sys.setenv(DUCKPLYR_FORCE = TRUE)
+Sys.setenv(DUCKPLYR_META_SKIP = TRUE)
+# Sys.setenv(DUCKPLYR_OUTPUT_ORDER = TRUE) # Too complex, need to move OO preservation to R layer of duckdb
