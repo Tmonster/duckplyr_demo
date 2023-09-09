e239312d2e2def1e7014000455b1ebbc96d2fcc5
Kirill Müller <kirill@cynkra.com>
2023-09-09 21:38:14 +0200

    Add demo for video

---
+++ video.R
@@ -0,0 +1,36 @@
+library(conflicted)
+library(duckplyr)
+conflicts_prefer(duckplyr::filter)
+
+taxi_data <- duckplyr_df_from_file(
+  "taxi-data-2019-partitioned/*/*.parquet",
+  "read_parquet",
+  list(hive_partitioning = TRUE)
+)
+
+taxi_data_prep <-
+  taxi_data |>
+  filter(total_amount > 0) |>
+  mutate(tip_pct = 100 * tip_amount / total_amount)
+
+tips_by_passenger <-
+  taxi_data_prep |>
+  summarize(
+    .by = passenger_count,
+    avg_tip_pct = median(tip_pct),
+    n = n(),
+  ) |>
+  arrange(desc(passenger_count))
+
+class(tips_by_passenger)
+
+duckdb:::rel_explain(
+  duckdb:::rel_from_altrep_df(tips_by_passenger)
+)
+
+system.time(print(tips_by_passenger$passenger_count))
+system.time(print(tips_by_passenger$passenger_count))
+
+tips_by_passenger
+
+system.time(nrow(taxi_data))
