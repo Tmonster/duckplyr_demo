33be01110008b0b0128dd6911b196abf5c905e16
Kirill Müller <kirill@cynkra.com>
2023-09-09 21:36:56 +0200

    Three months to avoid dplyr errors

--- duckplyr/load_taxi_data.R
+++ duckplyr/load_taxi_data.R
@@ -1,6 +1,16 @@
-if (!exists("taxi_data_2019") && !exists("zone_map")) {
-  taxi_data_2019 <- as_duckplyr_df(duckdb:::sql("FROM 'taxi-data-2019-partitioned/*/*.parquet'"))
-  zone_map <- as_duckplyr_df(duckdb:::sql("FROM 'zone_lookups.parquet'"))
+if (!exists("taxi_data_2019")) {
+  taxi_data_2019 <- as_duckplyr_df(
+    duckdb:::sql("FROM 'taxi-data-2019-partitioned/*/*.parquet' WHERE month IN (10, 11, 12)")
+  )
+}
+
+if (!exists("zone_map")) {
+  zone_map <- as_duckplyr_df(
+    duckdb:::sql("FROM 'zone_lookups.parquet'")
+  )
+}
+
+if (!exists("taxi_data_2019_lazy")) {
   taxi_data_2019_lazy <- duckplyr_df_from_file("taxi-data-2019-partitioned/*/*.parquet", "read_parquet", list(hive_partitioning = TRUE))
 }
 
